97af17022ee17a68302b620b3a0873ac6d0ccaff
CodeAshaq <havizhashaq@gmail.com>
2022-12-26 19:13:57 +0700

    perlahan

---
+++ lib/detail_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                )),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: const [
+                      Icon(Icons.calendar_today),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Text('Open Everyday')
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Icon(Icons.lock_clock),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Text('09.00 - 21.00')
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Icon(Icons.monetization_on),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Text('Rp 25000')
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:aplikasi_wisata_bandung/detail_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(
+      ),
+      home: const DetailScreen(),
+    );
+  }
+}
+
